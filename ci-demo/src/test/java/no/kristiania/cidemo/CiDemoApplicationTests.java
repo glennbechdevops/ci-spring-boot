baa5ef96ff3a54589557245d37c9327782868a04
Ola Nordmann <ola@example.com>
2026-09-11 12:32:17 +0000

    Initial commit from Spring Initializr

---
+++ ci-demo/src/test/java/no/kristiania/cidemo/CiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kristiania.cidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
